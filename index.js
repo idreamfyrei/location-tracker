083a74edc5dc6b010fc4b334405521fbd81a3adc
idreamfyrei <saumya.x3@gmail.com>
2026-05-03 01:30:10 +0530

    feat: topics and consumer

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import path from "node:path";
 import express from "express";
 import { Server } from "socket.io";
 import "dotenv/config";
+import { kafkaClient } from "./kafka-client.js";
 
 async function main() {
   const __filename = fileURLToPath(import.meta.url);
@@ -15,6 +16,29 @@ async function main() {
   const server = http.createServer(app);
 
   const io = new Server();
+
+  const kafkaProducer = kafkaClient.producer();
+  await kafkaProducer.connect();
+
+  const kafkaConsumer = kafkaClient.consumer({
+    groupId: `socket-server-${PORT}`,
+  });
+  await kafkaConsumer.connect();
+
+  await kafkaConsumer.subscribe({
+    topics: ["location-updates"],
+    fromBeginning: true,
+  });
+
+  kafkaConsumer.run({
+    eachMessage: async ({ topic, partition, message, heartbeat }) => {
+      const data = JSON.parse(message.value.toString());
+      console.log(`kafka consumer data received`, { data });
+      io.emit("server:location:update", { id: data.id, latitude: data.latitude, longitude: data.longitude });
+      await heartbeat();
+    },
+  });
+
   io.attach(server);
 
   io.on("connection", (socket) => {
@@ -22,9 +46,25 @@ async function main() {
 
     socket.on("user:location:update", (locationData) => {
       const { latitude, longitude } = locationData;
-        console.log(
-            `[Socket:$(socket.id)]:user:location:update: location updated to`, latitude, longitude
+      console.log(
+        `[Socket:$(socket.id)]:user:location:update: location updated to`,
+        latitude,
+        longitude,
       );
+
+      kafkaProducer.send({
+        topic: "location-updates",
+        messages: [
+          {
+            key: socket.id,
+            value: JSON.stringify({
+              id: socket.id,
+              latitude,
+              longitude,
+            }),
+          },
+        ],
+      });
     });
   });
 
